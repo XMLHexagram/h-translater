47153195346853d59a8f418664def2a20e3794ec
Hexagram <lmx_Hexagram@outlook.com>
2020-07-20 21:00:24 +0800

    test tailwind

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 // import './registerServiceWorker'
 import router from './router'
 import store from './store/store'
-import './assets/styles/index.css'
+import './assets/tailwind.css'
 
 Vue.config.productionTip = false
 
