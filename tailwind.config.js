356b14e29aed4ff03b6e718a9e646afb764e2d4d
Hexagram <lmx_Hexagram@outlook.com>
2020-07-21 20:27:00 +0800

    modify project config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {}
+    extend: {
+      gridTemplateRows: {
+        '9': 'repeat(9, minmax(0, 1fr))'
+      },
+      height: {
+        '1/10': '10%',
+        '9/10': '90%',
+        '1/8': '12.5%',
+        '7/8': '87.5%',
+        '1/7': '14.2857%',
+        '6/7': '85.7142%'
+      }
+    }
   },
   variants: {},
   plugins: []
