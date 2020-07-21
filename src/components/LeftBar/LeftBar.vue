f2991b7282f9924ba054d176732345668b8a327e
Hexagram <lmx_Hexagram@outlook.com>
2020-07-21 20:31:33 +0800

    add left bar

---
+++ src/components/LeftBar/LeftBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="w-full h-full flex flex-col bg-gray-600">
+    <div
+      class="h-1/10 flex justify-center items-center border-gray-900 border-b-2"
+    >
+      <slot name="header">H-transter</slot>
+    </div>
+
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+// import LeftBarItem from './LeftBarItem'
+export default {
+  name: 'LeftBar',
+
+  // components: { LeftBarItem },
+
+  provide() {
+    return {
+      rootLeftBar: this
+    }
+  },
+
+  data() {
+    return {
+      activeID: null
+    }
+  },
+
+  methods: {
+    // eslint-disable-next-line no-unused-vars
+    changeActive(id) {
+      for (const item of this.activeControl) {
+        this.activeControl.set(item[0], true)
+      }
+      console.log(this.activeControl)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/LeftBar/LeftBarItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    v-on:click="afterClicked"
+    class="default"
+    :class="[isActive ? 'actived' : 'before-active']"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeftBarItem',
+
+  inject: ['rootLeftBar'],
+
+  props: {
+    ID: String,
+    router: String
+  },
+
+  data() {
+    return {}
+  },
+
+  methods: {
+    afterClicked() {
+      this.rootLeftBar.activeID = this.ID
+      let router = this.router || this.ID
+      this.$router.push(router)
+    }
+  },
+
+  computed: {
+    isActive() {
+      return this.ID === this.rootLeftBar.activeID
+    }
+  }
+}
+</script>
+
+<style scoped>
+.default {
+  @apply flex justify-center items-center flex-none text-gray-600 h-12;
+}
+
+.default:hover {
+  /* @apply bg-gray-700; */
+}
+
+.actived {
+  @apply bg-gray-800 text-gray-200;
+}
+
+.before-active {
+  @apply bg-gray-600 text-gray-300;
+}
+</style>
